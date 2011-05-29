de3981323eac6c9205fc0034ae80784937ed6093
Shayne Czyzewski <thelastemail@gmail.com>
2011-05-29 15:12:46 -0500

    added saving results to db

--- Marry-Murder-Mate/create.sql
+++ Marry-Murder-Mate/create.sql
@@ -1,13 +1,21 @@
-# DON'T USE JUST YET! Not sure this is what we need... will finalize shortly. :)
+CREATE DATABASE mmm;
+
+USE mmm;
 
 CREATE TABLE mmm_votes (
-  id INT NOT NULL AUTO_INCREMENT,
-  added DATETIME,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  added TIMESTAMP DEFAULT NOW(),
   voter_id BIGINT,
   user1_id BIGINT,
   vote1 ENUM('Marry', 'Murder', 'Mate'),
   user2_id BIGINT,
   vote2 ENUM('Marry', 'Murder', 'Mate'),
   user3_id BIGINT,
-  vote3 ENUM('Marry', 'Murder', 'Mate')
-);
+  vote3 ENUM('Marry', 'Murder', 'Mate'),
+  INDEX(voter_id),
+  INDEX(user1_id),
+  INDEX(user2_id),
+  INDEX(user3_id)
+) ENGINE = InnoDB;
+
+ALTER TABLE mmm_votes ADD UNIQUE(voter_id, user1_id, user2_id, user3_id);
