1c3e0c8e580186efb0202c11a043a3a256e9c10c
Shayne Czyzewski <thelastemail@gmail.com>
2011-05-30 15:42:07 -0500

    result size check for empty vote history added

--- Marry-Murder-Mate/random-game.php
+++ Marry-Murder-Mate/random-game.php
@@ -121,43 +121,44 @@ if ($user) {
         <?php
         // populate for fql
         $result = mysql_query("SELECT * FROM mmm_votes WHERE voter_id = $user ORDER BY added DESC");
-        $user_ids_all = array();
-        while ($row = mysql_fetch_array($result)) {
-          array_push($user_ids_all, $row['user1_id'], $row['user2_id'], $row['user3_id']);
+        if (mysql_num_rows($result) > 0) {
+          $user_ids_all = array();
+          while ($row = mysql_fetch_array($result)) {
+            array_push($user_ids_all, $row['user1_id'], $row['user2_id'], $row['user3_id']);
+          }
+          $user_ids = array_unique($user_ids_all);
+          $user_names = getNames($facebook, $user_ids);
+          $user_pics = getPictures($facebook, $user_ids);
+          
+          // display
+          mysql_data_seek($result, 0);
+          while ($row = mysql_fetch_array($result)) {
+            ?>
+            <div class="person">
+              <div class="name"><?php echo $user_names[$row['user1_id']]; ?></div>
+              <div class="pic"><img src="<?php echo $user_pics[$row['user1_id']]; ?>"></div>
+              <div style="font-weight: bold; text-align: center;">
+                <?php echo $row['vote1']; ?>
+              </div>
+            </div>
+            <div class="person">
+              <div class="name"><?php echo $user_names[$row['user2_id']]; ?></div>
+              <div class="pic"><img src="<?php echo $user_pics[$row['user2_id']]; ?>"></div>
+              <div style="font-weight: bold; text-align: center;">
+                <?php echo $row['vote2']; ?>
+              </div>
+            </div>
+            <div class="person">
+              <div class="name"><?php echo $user_names[$row['user3_id']]; ?></div>
+              <div class="pic"><img src="<?php echo $user_pics[$row['user3_id']]; ?>"></div>
+              <div style="font-weight: bold; text-align: center;">
+                <?php echo $row['vote3']; ?>
+              </div>
+            </div>
+            <br style="clear:both"/>
+            <?php
+          }
         }
-        $user_ids = array_unique($user_ids_all);
-        $user_names = getNames($facebook, $user_ids);
-        $user_pics = getPictures($facebook, $user_ids);
-        
-        // display
-        mysql_data_seek($result, 0);
-        while ($row = mysql_fetch_array($result)) {
-          ?>
-          <div class="person">
-            <div class="name"><?php echo $user_names[$row['user1_id']]; ?></div>
-            <div class="pic"><img src="<?php echo $user_pics[$row['user1_id']]; ?>"></div>
-            <div style="font-weight: bold; text-align: center;">
-              <?php echo $row['vote1']; ?>
-            </div>
-          </div>
-          <div class="person">
-            <div class="name"><?php echo $user_names[$row['user2_id']]; ?></div>
-            <div class="pic"><img src="<?php echo $user_pics[$row['user2_id']]; ?>"></div>
-            <div style="font-weight: bold; text-align: center;">
-              <?php echo $row['vote2']; ?>
-            </div>
-          </div>
-          <div class="person">
-            <div class="name"><?php echo $user_names[$row['user3_id']]; ?></div>
-            <div class="pic"><img src="<?php echo $user_pics[$row['user3_id']]; ?>"></div>
-            <div style="font-weight: bold; text-align: center;">
-              <?php echo $row['vote3']; ?>
-            </div>
-          </div>
-          <br style="clear:both"/>
-          <?php
-        }
-        
         mysql_free_result($result)
         ?>
       </div>
